ca92896f491646d31d40dcdfe3bccaa7e9558035
wjh <wjh@travelsky.com.cn>
2024-11-15 13:40:30 +0800

    mem0学习

---
+++ inbox/mem0/mem0_hello_world.py
@@ -0,0 +1,21 @@
+from mem0 import MemoryClient
+
+m = MemoryClient(api_key="m0-")
+
+# 从任何非结构化文本中存储记忆
+result = m.add("我正在学习python,请推荐一些学习课程", user_id="wjh", metadata={"category": "study"})
+print(result)
+# 创建的记忆：正在学习Python
+result = m.add("我喜欢玩switch双人游戏", user_id="wjh", metadata={"category": "hobbies"})
+print(result)
+# 创建的记忆：喜欢玩Switch双人游戏
+
+# 搜索记忆
+related_memories = m.search(query="wjh 最近在干嘛?", user_id="wjh")
+print(related_memories)
+related_memories = m.search(query="wjh 喜欢玩啥?", user_id="wjh")
+print(related_memories)
+
+# 获取记忆历史
+all_memories = m.get_all(user_id="wjh")
+print(all_memories)
