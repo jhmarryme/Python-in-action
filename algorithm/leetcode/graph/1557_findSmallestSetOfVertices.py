a3c7ddb6fe962363816a1ba9544d8ee52bb31453
wjh <wjh@travelsky.com.cn>
2025-10-15 09:22:28 +0800

    refactor(graph):优化1557题解法，使用入度统计替代BFS
    - 移除不必要的BFS实现和邻接表构建
    - 采用统计节点入度的方式直接求解

--- algorithm/leetcode/graph/1557_findSmallestSetOfVertices.py
+++ algorithm/leetcode/graph/1557_findSmallestSetOfVertices.py
@@ -1,35 +1,17 @@
-import collections
-from collections import deque
 from typing import List
 
 
 class Solution:
+    """
+    1557. 可以到达所有点的最少点数目
+    """
+
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        adj_list = collections.defaultdict(list)
-        for x, y in edges:
-            adj_list[x].append(y)
-
-        def bfs(start):
-            component = list()
-            visited = set()
-            visited.add(start)
-            queue = deque([start])
-            while queue:
-                node = queue.popleft()
-                component.append(node)
-                for neighbor in adj_list[node]:
-                    if neighbor not in visited:
-                        visited.add(neighbor)
-                        queue.append(neighbor)
-            components.append(component)
-
-        components = list()
-        for k in range(n):
-            if len(adj_list[k]) > 0:
-                bfs(k)
-        print(components)
+        # 用数组会超时
+        degree = set(y for x, y in edges)
+        return [i for i in range(n) if i not in degree]
 
 
-n = 6
-edges = [[0, 1], [0, 2], [2, 5], [3, 4], [4, 2]]
-Solution().findSmallestSetOfVertices(n, edges)
+n = 5
+edges = [[0, 1], [2, 1], [3, 1], [1, 4], [2, 4]]
+print(Solution().findSmallestSetOfVertices(n, edges))
