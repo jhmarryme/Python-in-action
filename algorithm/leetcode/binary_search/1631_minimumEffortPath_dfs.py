17622370e5e9347182b1a90afa439be57c78f8b7
wjh <wjh@travelsky.com.cn>
2025-10-27 16:47:26 +0800

    feat(algorithm): 实现二分搜索与DFS解决最小体力消耗路径问题
    - 使用二分查找确定最小体力消耗值
    - 应用深度优先搜索验证路径可行性
    - 移除不必要的导入和变量声明- 更新测试用例以匹配新实现逻辑
    - 添加完整的问题描述和示例注释

--- algorithm/leetcode/binary_search/1631_minimumEffortPath_dfs.py
+++ algorithm/leetcode/binary_search/1631_minimumEffortPath_dfs.py
@@ -1,15 +1,30 @@
-from collections import deque
-from typing import List
-
-
-class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+class Solution(object):
+    def minimumEffortPath(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: int
+        """
         m, n = len(heights), len(heights[0])
-        lefres = 0
+        left, right, ans = 0, max([num for rows in heights for num in rows]), 0
+        while left <= right:
+            mid = (left + right) // 2
+            seen = set()
+
+            def dfs(i, j):
+                for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                    if 0 <= x < m and 0 <= y < n and (x, y) not in seen and abs(heights[i][j] - heights[x][y]) <= mid:
+                        seen.add((x, y))
+                        dfs(x, y)
+
+            seen.add((0, 0))
+            dfs(0, 0)
+            if (m - 1, n - 1) in seen:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
 
 
-        return res
-
-
-heights = [[1, 2, 1, 1, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 2, 1, 2, 1], [1, 1, 1, 2, 1]]
+heights = [[1, 2, 2], [3, 8, 2], [5, 3, 5]]
 print(Solution().minimumEffortPath(heights))
