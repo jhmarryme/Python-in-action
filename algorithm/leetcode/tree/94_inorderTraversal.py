d96929e2eeefc07f8f8def32be55fdad458897e7
wjh <wjh@travelsky.com.cn>
2025-10-28 13:44:29 +0800

    feat(tree): 添加二叉树的中序、前序和后序遍历实现
    - 实现了中序遍历算法 (94_inorderTraversal.py)
    - 实现了前序遍历算法 (144_preorderTraversal.py)
    - 实现了后序遍历算法 (145_postorderTraversal.py)- 所有实现均支持空树和单节点树的情况
    - 包含详细的题目描述和示例输入输出- 使用递归方式实现深度优先搜索
    - 添加了进阶提示，为后续迭代算法留空间

---
+++ algorithm/leetcode/tree/94_inorderTraversal.py
@@ -0,0 +1,66 @@
+# 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+#
+#
+#
+#  示例 1：
+#
+#
+# 输入：root = [1,null,2,3]
+# 输出：[1,3,2]
+#
+#
+#  示例 2：
+#
+#
+# 输入：root = []
+# 输出：[]
+#
+#
+#  示例 3：
+#
+#
+# 输入：root = [1]
+# 输出：[1]
+#
+#
+#
+#
+#  提示：
+#
+#
+#  树中节点数目在范围 [0, 100] 内
+#  -100 <= Node.val <= 100
+#
+#
+#
+#
+#  进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+#
+#  Related Topics 栈 树 深度优先搜索 二叉树 👍 2321 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        res = list()
+
+        def inOrder(node):
+            if not node:
+                return
+            inOrder(node.left)
+            res.append(node.val)
+            inOrder(node.right)
+
+        inOrder(root)
+        return res
+# leetcode submit region end(Prohibit modification and deletion)
